be90582fc84e5f987a42ea6bdca924ea254ba917
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.eu>
2023-11-09 02:23:16 +0100

    FEAT: setup the main logic of the program

---
+++ include/bsq.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** bsq.h
+** File description:
+** Necesary functions and structs
+*/
+
+#ifndef BSQ_H_
+    #define BSQ_H_
+
+    #include <stdlib.h>
+
+typedef struct board_data{
+    size_t width;
+    size_t height;
+    char **map;
+} board;
+
+board *board_from_file(char const *file);
+board *generate_board(char const *size, char const *pattern);
+void print_board(board *b);
+void destroy_board(board *b);
+
+size_t bsq(board *b);
+
+#endif
